3c3866dd43c866c12b3303a46c1d87823e82f8d2
Aniket Dhakate <Aniket30-ai>
2024-07-31 14:34:43 +0530

    create a story upadete 300

--- src/main/java/com/example/demo/MobileBanking1Application.java
+++ src/main/java/com/example/demo/MobileBanking1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MobileBanking1Application {
-
+public static final int a=10;
 	public static void main(String[] args) {
 		SpringApplication.run(MobileBanking1Application.class, args);
 	}
